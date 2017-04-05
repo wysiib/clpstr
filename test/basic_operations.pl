e5da1f7087841a2e2f43e1bec470f1dcd38a4457
Sebastian Krings <sebastian@krin.gs>
2017-04-05 13:36:32 +0200

    add testcases

---
+++ test/basic_operations.pl
@@ -0,0 +1,16 @@
+:- use_module(library(plunit)).
+
+:- use_module('../src/domains/basic_domains').
+:- use_module('../src/domains/basic_operations').
+
+:- begin_tests(constant_domains).
+
+test(constant_domain_intersection_same,[true(D == D1)]) :-
+  constant_string_domain("abc",D1),
+  intersection(D1,D1,D).
+test(constant_domain_intersection_different,[true(D == empty)]) :-
+  constant_string_domain("abc",D1),
+  constant_string_domain("def",D2),
+  intersection(D1,D2,D).
+
+:- end_tests(constant_domains).
