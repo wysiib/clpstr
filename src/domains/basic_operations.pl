8b9d2f516d614b295f094caa70afb7e0ffe503e2
Malte Schmidt <malte.schmidt@uni-duesseldorf.de>
2017-04-05 13:20:22 +0200

    added intersection to the list of basic operations.

--- src/domains/basic_operations.pl
+++ src/domains/basic_operations.pl
@@ -1,2 +1,11 @@
-:- module(basic_operations,[repeat/4,
-                            intersection/3]).
+:- module(basic_operations,[is_empty/1,
+          repeat/4, intersection/3]).
+
+%! Intersection(String1,String2,ResultingDomain) is det
+% Generates a domain from the intersection of two strings.
+% @String1 is the first string.
+% @String2 is the second string.
+% @ResultingDomain is the domain containing the Intersection of the two strings.
+intersection(S1,S2,Inter):-
+  S1=S2,
+  Inter = S1.
