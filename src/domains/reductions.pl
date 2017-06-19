488cb93488d3211b2be4bb178dd361aa6e16ace0
Malte Schmidt <malte.schmidt@uni-duesseldorf.de>
2017-06-19 14:56:10 +0200

    add documentary to epsilon closure

--- src/domains/reductions.pl
+++ src/domains/reductions.pl
@@ -8,7 +8,21 @@ epsilon_reduce(_,_) :- fail.
 dfa_reduce(_,_) :- fail.
 
 
-
+%! epsilon_closure(StartState,TransitionList,EpsilonClosure) is det
+% Takes a state and a list of transitions and returns all states reachable
+% by only using epsilon transitions, starting in StartState. It does not
+% include StartState in the closure, when there is no explicit epsilon
+% transition in the list.
+% NOTE that this is not a formaly correct epsilon closure, since it does not
+% include, although it is obviously always reachable via an epsilon
+% transition from the initial state.
+% Also if this ever breaks, its propably because it does not test whether
+% S is actually included in T.
+% @StartState is the state from which the epsilon closure shall be calculated.
+% @TransitionList shall be initiialized by the List of reachable transitions
+% by StartState.
+% @EpsilonClosure is the resulting list of states representing the epsilon
+% closure of StartState.
 epsilon_closure(S,[TH|TT],[Next|ResT]) :-
   TH = (S,epsilon,Next),
   !,
