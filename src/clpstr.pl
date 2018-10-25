903e248946e946205d86d8f22dbc7d8334529ea3
Malte Schmidt <malte.schmidt@uni-duesseldorf.de>
2018-10-25 12:46:54 +0200

    add comments to clpstr

--- src/clpstr.pl
+++ src/clpstr.pl
@@ -1,17 +1,17 @@
  :- module(clpstr,[str_in/2,
-                   word_size/2,
-                   clp_repeat/1,
-                   clp_repeat/2,
-                   clp_repeat/3,
-                   clp_intersection/3]).
+                   str_size/2,
+                   str_repeat/1,
+                   str_repeat/2,
+                   str_repeat/3,
+                   str_intersection/3]).
 
 :- use_module(library(chr)).
 
 :- use_module('domains/basic_operations').
 :- use_module('domains/labeling').
 
-:- chr_constraint str_in/2, str_labeling/1, clp_size/2, clp_repeat/1,
-   clp_repeat/2, clp_repeat/3, clp_intersection/3.
+:- chr_constraint str_in/2, str_labeling/1, str_size/2, str_repeat/1,
+   str_repeat/2, str_repeat/3, str_intersection/3.
 
 % chr rule wakes up each time a new or updated str_in is added
 % in case the domain is empty, no sulution is possible anymore:
@@ -47,22 +47,22 @@ var_is_member(X,[_|T]) :- var_is_member(X,T).
 % If you already know Domain, set it up to be in the intersection of the two
 % domains. If not set it up to be as a new domain.
 % Dismiss old domain and constraint.
-clp_size(X,I), str_in(X,D1)
+str_size(X,I), str_in(X,D1)
          <=> integer(I) | generate_any_size(D2,I),
              intersection(D1,D2,D3), stri_in(X,D3).
-clp_size(X,I) <=> integer(I) | generate_any_size(D,I), stri_in(X,D).
+str_size(X,I) <=> integer(I) | generate_any_size(D,I), stri_in(X,D).
 
 
 % Take the variable  and repeat it the specific amount of times.
 % Dismiss old domain and constraint.
-clp_repeat(X), str_in(X,D1)
+str_repeat(X), str_in(X,D1)
         <=> repeat(D1,D2), str_in(X,D2).
-clp_repeat(X,Nmb), str_in(X,D1)
+str_repeat(X,Nmb), str_in(X,D1)
         <=> repeat(D1,Nmb,D2), str_in(X,D2).
-clp_repeat(X,From,To), str_in(X,D1)
+str_repeat(X,From,To), str_in(X,D1)
         <=> repeat(D1,From,To,D2), str_in(X,D2).
 
 % Take 3 variables and calc the intersection of the three.
 % Dismiss the constraint and keep the str_in of the other var.
-clp_intersection(X1,X2,X3) \ str_in(X2,D2), str_in(X3,D3)
+str_intersection(X1,X2,X3) \ str_in(X2,D2), str_in(X3,D3)
         <=> intersection(D2,D3,D1), str_in(X1,D1).
