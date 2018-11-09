fdbe96ee2a1afb8cc5f71ddae56782360df47e57
Malte Schmidt <malte.schmidt@uni-duesseldorf.de>
2018-11-09 11:42:19 +0100

    ready tests for labeling

--- bm/test_framework.pl
+++ bm/test_framework.pl
@@ -1,19 +1,21 @@
 :- use_module('../src/clpstr').
 :- dynamic benchmark/1.
 
-
 make_all_benchmarks(Directory,Name) :-
+  make_all_benchmarks(Directory,Name,[]).
+
+make_all_benchmarks(Directory,Name,Options) :-
   directory_files(Directory,Files),
   %writeln(Files),
-  maplist(benchmark_test(Name,Directory),Files).
+  maplist(benchmark_test(Name,Directory,Options),Files).
 
-benchmark_test(_,_,.) :- !.
-benchmark_test(_,_,..) :- !.
-benchmark_test(ResultName,DirectoryName,FileName) :-
+benchmark_test(_,_,_,.) :- !.
+benchmark_test(_,_,_,..) :- !.
+benchmark_test(ResultName,DirectoryName,Options,FileName) :-
   atom_concat(DirectoryName,'/',Directory),
   atom_concat(Directory,FileName,FileLocation),
   consult(FileLocation),
-  run_benchmark([],Time,Inferences),!,
+  run_benchmark(Options,Time,Inferences),!,
   retractall(benchmark(_)),
   write_data(FileName,ResultName,Time,Inferences).
 
