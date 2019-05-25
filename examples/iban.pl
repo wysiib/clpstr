d0550afdb8c34769d0e0937d5a5ce5dbe5e017d1
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2019-05-25 13:45:50 +0200

    update iban example: does not scale since concat is slow

--- examples/iban.pl
+++ examples/iban.pl
@@ -1,26 +1,23 @@
 :- use_module('../src/clpstr').
 :- use_module(library(clpfd)).
 
-
 iban :-
-    str_in(DECode,"1314"),
-    BBAN in 000000000000000000 ..999999999999999999,
-    str_to_int(BBANStr,BBAN),
-    % for the "00" to have a domain, we need to set it up independently
-    str_in(DoubleZero,"00"),
-    str_concatenation(DECode,DoubleZero,ICalcTail),
-    str_concatenation(BBANStr,ICalcTail,ICalc),
-    ICalcInt in 000000000000000000000000..999999999999999999999999,
-    str_to_int(ICalc,ICalcInt),
-    Rest in 0..96,
-    ICalcInt mod 97 #= Rest,
-    str_in(CheckSumStr,"[0-9][0-9]"),
-    98 - Rest #= CheckSumInt,
-    %str_to_int(CheckSumStr, CheckSumInt),
-    str_in(DEConst,"DE"), % ensure this has a domain
-    str_concatenation(DEConst,CheckSumStr,IBANPrefix),
-    %str_in(IBAN, "DE[0-9]*"),
-    %str_size(IBAN, 22),
-    str_concatenation(IBANPrefix,BBANStr,IBAN),
-    str_labeling([],[BBAN,ICalc,CheckSumInt,BBANStr,DE,ICalcTail,IBANPrefix,IBAN]),
-    format('iban found: ~w~n',[IBAN]).
+    Rest in 10..96,
+    ICalc in 100000000000000000131400..999999999999999999131499,
+    ICalc mod 97 #= Rest,
+    clpfd:labeling([], [ICalc, Rest]),
+    str_in(BBANStr, "[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]"),
+    str_in(DECode, "1314"),
+    str_in(CheckSumStr, "[0-9][0-9]"),
+    str_to_int(CheckSumStr, Rest),
+    str_to_int(StrCalc, ICalc),
+    str_in(DECodeCheckSum, "[0-9][0-9][0-9][0-9][0-9][0-9]"),
+    str_concatenation(BBANStr, DECodeCheckSum, StrCalc),
+    str_labeling([],[BBANStr, DECodeCheckSum, StrCalc]),
+    str_concatenation(DECode, CheckSumStr, DECodeCheckSum),
+    str_in(IBAN, "[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]"),
+    str_in(DE, "DE"),
+    str_in(Prefix, "DE[0-9][0-9]"),
+    str_concatenation(DE, CheckSumStr, Prefix),
+    str_concatenation(Prefix, BBANStr, IBAN),
+    str_labeling([], [CheckSumStr, Prefix, IBAN]).
