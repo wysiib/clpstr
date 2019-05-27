6cb79ef8d383989d414a67f9b2e5b520895589be
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2019-05-27 17:32:17 +0200

    update iban example

--- examples/iban.pl
+++ examples/iban.pl
@@ -3,13 +3,13 @@
 
 iban(IBAN) :-
     Rest in 0..96,
-    ICalc in 100000000000000000..999999999999999999,
-    ICalc2 #= ICalc * 1000000 + 131400, % ICalc2 has the constant suffix 131400
-    ICalc2 mod 97 #= Rest,
-    clpfd:labeling([],[ICalc2, Rest]),
+    TICalc in 100000000000000000..999999999999999999,
+    ICalc #= TICalc * 1000000 + 131400, % ICalc has the constant suffix 131400
+    ICalc mod 97 #= Rest,
+    str_label([ICalc, Rest]),
     str_in(BBANStr, "[0-9]{18}"),
     str_in(DECodeCheckSumStr, "[0-9]{6}"),
-    str_to_int(StrCalc, ICalc2),
+    str_to_int(StrCalc, ICalc),
     str_concatenation(BBANStr, DECodeCheckSumStr, StrCalc),
     str_in(IBAN, "DE[0-9]{20}"),
     str_in(DE, "DE"),
@@ -17,4 +17,4 @@ iban(IBAN) :-
     str_to_int(CheckSumStr, CheckSum),
     str_concatenation(DE, CheckSumStr, IBANPrefix),
     str_concatenation(IBANPrefix, BBANStr, IBAN),
-    str_labeling([], [IBAN]). % [ICalc2, Rest, IBAN] clpfd variables can also be labeled here which, however, is a bit slower
+    str_label([IBAN]). % [ICalc, Rest, IBAN] clpfd variables can also be labeled here which, however, is a bit slower
