471fd77d9c855cd55c36dbd2b1d38fb8880838d2
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2019-05-31 09:54:47 +0200

    Add calendar date example

---
+++ examples/full_date_language.pl
@@ -0,0 +1,47 @@
+:- use_module('../src/clpstr').
+
+% This code captures the domain of valid date notations
+% from January 1 1 to December 31 9999.
+%
+% Possible notations are of the forms
+% - August 28
+% - August 28, 1996
+% - Wednesday
+% - Wednesday, August 28
+% - Wednesday, August 28, 1996
+%
+% Regex taken and adapted from "Regular expressions for language engineering"
+% @article{karttunen1996regular,
+%   title={Regular expressions for language engineering},
+%   author={Karttunen, Lauri and Chanod, Jean-Pierre and Grefenstette, Gregory and Schille, Anne},
+%   journal={Natural Language Engineering},
+%   volume={2},
+%   number={4},
+%   pages={305--328},
+%   year={1996},
+%   publisher={Cambridge University Press}
+% }
+
+
+input_gen(DateDom) :-
+  % Basics
+  str_in(DayNames,
+    "Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday"),
+  str_in(MonthNames,
+    "January|February|March|April|June|July|August|September|October|November|December"),
+  str_in(DayNum, "[1-9]|[1-2][0-9]|3[0-1]"),
+  str_in(Year,"[1-9][0-9]{0,3}"),
+  str_in(Sep, "_"),
+  str_in(CSep, ",_"),
+  % Construction
+  str_concatenation(DayNames, CSep, DaySep),
+  str_concatenation(MonthNames, Sep, MonthSep),
+  str_concatenation(MonthSep, DayNum, MonthDay),
+  str_concatenation(MonthDay, CSep, MonthDaySep),
+  str_concatenation(MonthDaySep, Year, MonthDayYear),
+  str_union(MonthDayYear, MonthDay, MonthDayOptYear),
+  str_concatenation(DaySep, MonthDayOptYear, FullDate),
+  str_union(MonthDayOptYear, FullDate, FullDateOptDay),
+  str_union(FullDateOptDay, DayNames, DateDom),
+  % Labeling
+  str_labeling([], [DateDom]).
