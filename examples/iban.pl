ae68a61b3ab2b59f4f2d66056844827669000099
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2019-06-24 08:17:13 +0200

    update iban example

--- examples/iban.pl
+++ examples/iban.pl
@@ -25,7 +25,24 @@ benchmarks(Amount) :-
         format("Total walltime for ~w IBANs: ~w ms", [Amount, TotalR])
     ;   fail).
 
+% fastest
 iban(IBAN) :-
+    Rest in 0..96,
+    TICalc in 100000000000000000..999999999999999999,
+    ICalc #= TICalc * 1000000 + 131400, % ICalc has the constant suffix 131400
+    ICalc mod 97 #= Rest,
+    str_label([ICalc, Rest]),
+    BBAN is ICalc // 1000000,
+    str_to_int(BBANStr, BBAN),
+    CheckSum #= 98 - Rest,
+    str_to_intl(CheckSumStr, CheckSum),
+    str_size(CheckSumStr, 2), % TODO: without size constraint we possibly find a solution several times
+    str_in(DE, "DE"),
+    str_concatenation(DE, CheckSumStr, IBANPrefix),
+    str_concatenation(IBANPrefix, BBANStr, IBAN),
+    str_label([IBAN]).
+
+iban2(IBAN) :-
     Rest in 0..96,
     TICalc in 100000000000000000..999999999999999999,
     ICalc #= TICalc * 1000000 + 131400, % ICalc has the constant suffix 131400
@@ -44,7 +61,7 @@ iban(IBAN) :-
     str_concatenation(IBANPrefix, BBANStr, IBAN),
     str_label([IBAN]). % [ICalc, Rest, IBAN] clpfd variables can also be labeled here which, however, is a bit slower
 
-iban2(IBAN) :-
+iban3(IBAN) :-
     Rest in 0..96,
     TICalc in 100000000000000000..999999999999999999,
     ICalc #= TICalc * 1000000 + 131400, % ICalc has the constant suffix 131400
@@ -57,4 +74,4 @@ iban2(IBAN) :-
     str_to_int(CheckSumStr, CheckSum),
     str_in(IBAN, "DE[0-9]{20}"),
     IBAN match "DE" + CheckSumStr + BBANStr,
-    str_label([IBAN]). % [ICalc, Rest, IBAN] clpfd variables can also be labeled here which, however, is a bit slower
+    str_label([IBAN]).
