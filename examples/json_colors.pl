604d6e23121cea3cf7cc338fef7ce1a515acee66
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2019-11-22 13:34:20 +0100

    update example generating data in JSON

--- examples/json_colors.pl
+++ examples/json_colors.pl
@@ -51,13 +51,13 @@ Generate data in JSON describing colors:
 list_of_hex_codes(0, []) :- !.
 list_of_hex_codes(C, [HexCode|T]) :-
     str_in(HexCode, "([A-F] | [0-9]){6}"),
-    C1 is C-1,
-    list_of_hex_codes(C1, T).
+    C1 is C-1, list_of_hex_codes(C1, T).
 
 get_color_entry(Hex, RgbList, Color) :-
     term_string(RgbList, Rgb1),
     escape_special_characters(Rgb1, Rgb),
-    Color match "\\{\"color\": \"test\",\"code\":\\{ \"rgb\": " + Rgb + ", \"hex\": #" + Hex + " \\}\\}".
+    Prefix = "\\{\"color\": \"test\",\"code\":\\{ \"rgb\": ",
+    Color match Prefix + Rgb + ", \"hex\": #" + Hex + " \\}\\}".
 
 list_of_colors_concat([], [], "").
 list_of_colors_concat([Hex|HT], [Rgb|RT], Concat) :-
@@ -67,16 +67,15 @@ list_of_colors_concat([Hex|HT], [Rgb|RT], Concat) :-
 list_of_colors_concat_acc([], [], Acc, Acc).
 list_of_colors_concat_acc([Hex|HT], [Rgb|RT], Acc, Concat) :-
     get_color_entry(Hex, Rgb, Color),
-    list_of_colors_concat_acc(HT, RT, '+'(Acc, '+'(",", Color)), Concat).
+    NewAcc = '+'(Acc, '+'(",", Color)),
+    list_of_colors_concat_acc(HT, RT, NewAcc, Concat).
 
-json_colors(Out) :-
-    random(0, 10, Amount),
-    format("~nAmount of colors: ~d~n", [Amount]),
+json_colors(Amount, Out) :-
     list_of_hex_codes(Amount, LHex),
-    str_all_diff(LHex),
+    str_all_diff(LHex),!,
     str_label(LHex),
     maplist(hex_bytes, LHex, RgbList),
     list_of_colors_concat(LHex, RgbList, ColorsConcat),
-    Json match "\\{\"colors\": \\[" + ColorsConcat + "\\]\\}",
-    str_label([Json]),
-    remove_escape_special_characters(Json, Out).
+    JSON match "\\{\"colors\": \\[" + ColorsConcat + "\\]\\}",
+    str_labeling([bfs], [JSON]),
+    remove_escape_special_characters(JSON, Out).
